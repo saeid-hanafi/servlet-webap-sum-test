94ef6cb5251c17a64a800cdb2915612598a31485
saeid-hanafi <saeed.hanafi.ciw@gmail.com>
2023-06-25 11:45:59 +0330

    initial-commit

---
+++ src/main/java/com/yasertebat/servlet/AddServlet.java
@@ -0,0 +1,42 @@
+package com.yasertebat.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(value = "/add")
+public class AddServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        String num1 = req.getParameter("num1");
+        String num2 = req.getParameter("num2");
+        if (isNum(num1) && isNum(num2)) {
+            double newNum1 = Double.parseDouble(num1);
+            double newNum2 = Double.parseDouble(num2);
+            double sum = newNum1 + newNum2;
+            writer.print(sum);
+        }else{
+            writer.print("Enter Valid Numbers, Please.");
+        }
+    }
+
+    public static boolean isNum(String num) {
+        if (num == null) {
+            return false;
+        }
+
+        try {
+            double newNum = Double.parseDouble(num);
+        }catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+<form action="/add">
+    <input type="text" name="num1" placeholder="Enter First Number...">
+    <input type="text" name="num2" placeholder="Enter Second Number...">
+    <input type="submit" name="sub" value="Add">
+</form>
+</body>
+</html>
